0683f38d3a2e5e36ead2de098d2c8738b4deb262
Adrianodisam <adrianodisam@gmail.com>
2023-01-04 22:27:53 -0300

    componente flutuante Whatsapp

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Footer from './componentes/Footer/Footer';
 import Header from './componentes/Header/Header';
 import Main from './componentes/Main/Main';
 import Navbar from './componentes/Navbar/Navbar';
+import WhatsAppContato from './componentes/WhatsAppContato/WhatsAppContato';
 
 function App() {
   return (
     <div className="App">
+      <WhatsAppContato texto="Agendar horário" />
       <Navbar />
       <Header />
       <Main />
